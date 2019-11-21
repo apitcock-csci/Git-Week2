dda9cde23c7fa7cb011228ec89b9a7a9c9871211
Anna Pitcock <annapitcock@csm-wl-dhcp-225-57.mines.edu>
2019-11-21 15:15:28 -0700

    First 100 numbers of fib

---
+++ fib.py
@@ -0,0 +1,14 @@
+def fib():
+    fibs = [1, 2]
+
+    for i in range(1,9):
+        fib_n = fibs[1] + fibs[i -1]
+        fibs.append(fib_n)
+
+    return fibs
+
+def main():
+    print('OUTPUT', fib())
+
+if __name__ == "__main__":
+    main()
